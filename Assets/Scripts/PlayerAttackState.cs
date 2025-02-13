c349f1a43c37c0ae3abc0e1723ea817dad83a713
blackpenguin <rimuru0a0@gmail.com>
2025-02-13 22:06:13 +0800

    [feat] PlayerAttackState

---
+++ Assets/Scripts/PlayerAttackState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAttackState : PlayerBaseState
+{
+    public override void EnterState(PlayerStateManager player)
+    {
+
+    }
+    
+    public override void UpdateState(PlayerStateManager player)
+    {
+
+    }
+    
+    public override void OncollisionEnter(PlayerStateManager player)
+    {
+       
+        
+    }
+}
--- Assets/Scripts/PlayerStateManager.cs
+++ Assets/Scripts/PlayerStateManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerStateManager : MonoBehaviour
 {
+    PlayerBaseState currentState;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
