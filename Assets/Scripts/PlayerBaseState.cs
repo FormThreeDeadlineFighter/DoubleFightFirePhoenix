a86dee270b5d603c84abb32222effd23d8aa5728
blackpenguin <rimuru0a0@gmail.com>
2025-02-14 15:03:04 +0800

    [feat] 玩家狀態機增加輸入增測

--- Assets/Scripts/PlayerBaseState.cs
+++ Assets/Scripts/PlayerBaseState.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public abstract class PlayerBaseState
 {
+    protected PlayerStateManager player;
+    public PlayerBaseState(PlayerStateManager player)
+    {
+        this.player = player;
+    }
     public abstract void EnterState(PlayerStateManager player);
     public abstract void UpdateState(PlayerStateManager player);
     public abstract void OncollisionEnter(PlayerStateManager player);
+    public abstract void HandleInput(Vector2 movementInput);
 }
