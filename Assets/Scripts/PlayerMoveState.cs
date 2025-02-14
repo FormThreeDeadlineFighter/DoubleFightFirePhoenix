dff31e7b1d8e23e1e027af7fa7c0a4c37aa56b8d
blackpenguin <rimuru0a0@gmail.com>
2025-02-14 15:09:51 +0800

    [feat] 玩家移動狀態機增加輸入增測

--- Assets/Scripts/PlayerMoveState.cs
+++ Assets/Scripts/PlayerMoveState.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMoveState : PlayerBaseState
 {
+    public PlayerMoveState(PlayerStateManager player) : base(player){}
     public override void EnterState(PlayerStateManager player)
     {
 
@@ -16,5 +17,13 @@ public class PlayerMoveState : PlayerBaseState
     public override void OncollisionEnter(PlayerStateManager player)
     {       
 
+    }
+
+    public override void HandleInput(Vector2 movementInput)
+    {       
+        if(movementInput.magnitude > 0.1f)
+        {
+            
+        }
     }    
 }
