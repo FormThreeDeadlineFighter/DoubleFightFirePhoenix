1558e74774a12e8c16bd1f5ce387adacc93c03b0
Poko0000 <pokopoko130000@gmail.com>
2025-02-22 18:01:40 +0800

    [modify] IStateController Update

--- Assets/Scripts/StateMachine/IStateController.cs
+++ Assets/Scripts/StateMachine/IStateController.cs
@@ -9,7 +9,7 @@ public class IStateController : MonoBehaviour
 
 
     // Update is called once per frame
-    protected void Update()
+    void Update()
     {
         _currentState.LogicUpdate();
     }
@@ -19,7 +19,7 @@ public class IStateController : MonoBehaviour
         _currentState.PhysicsUpdate();
     }
 
-    void SwitchOn(IState newState)
+    protected void SwitchOn(IState newState)
     {
         _currentState = newState;
         _currentState.EnterState();
