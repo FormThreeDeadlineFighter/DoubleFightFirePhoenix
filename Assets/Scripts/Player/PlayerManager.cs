fc624bbcad8ec817e9d5eb5a48e7d3070de4cd15
Poko0000 <pokopoko130000@gmail.com>
2025-04-01 16:52:35 +0800

    [Test] 標心Testing

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public void PlayerJoin()
+    {
+        //Debug.Log("player join");
+    }
+}
