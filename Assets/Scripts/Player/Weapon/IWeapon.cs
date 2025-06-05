d3f633b2f20adb2ad5d10607a333da1348dbf5af
Poko0000 <pokopoko130000@gmail.com>
2025-06-05 14:50:55 +0800

    modify: local change

--- Assets/Scripts/Player/Weapon/IWeapon.cs
+++ Assets/Scripts/Player/Weapon/IWeapon.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public abstract class IWeapon : MonoBehaviour
 {
-    
-    public GameObject weaponModel; //武器模型
-    public GameObject bulletPrefab; //子彈模型
-    public float damage; //武器傷害
-    public int maxAmmo; //最大子彈數
-    public float fireRate ; //射速
-    public float nextFireTime = 0f; //下一發射擊的冷卻時間
+    [Header("Info")]
+    string _name; //武器名字
+
+    [Header("Shoot")]
+    float damage; //武器傷害
+    int maxAmmo; //最大子彈數
+    float fireRate ; //射速
+    float nextFireTime = 0f; //下一發射擊的冷卻時間
+
+    [Header("Reload")]
     protected int currentAmmo; //當前擁有的子彈
     protected bool hasAmmo => currentAmmo > 0; //判斷是否有子彈
-    
     protected virtual void Reload() => currentAmmo = maxAmmo; //裝填子彈
     
     public abstract void InitializeWeapon(); 
-    public abstract void Attack(Transform firePoint);
-
-    public abstract void Attack(Transform firePoint, Transform enemyPosition);
+    public abstract void Attack();
 
 }
