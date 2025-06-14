e3c49c1ba39a3c31404dab34cb9942649dbd405d
Rimuru0a0 <99930843+Rimuru0a0@users.noreply.github.com>
2025-06-14 20:15:38 +0800

    feat: level 1 done

--- Assets/Scripts/Enemy/Enemys/Enemy.cs
+++ Assets/Scripts/Enemy/Enemys/Enemy.cs
@@ -1,46 +1,46 @@
 using UnityEngine;
 
 
-[RequireComponent( typeof(FollowPlayer))]
-public class Enemy :  IEnemy
-{    
+[RequireComponent(typeof(FollowPlayer))]
+public class Enemy : IEnemy
+{
     private FollowPlayer follower;
 
     void Awake()
     {
-        follower = transform.GetComponent<FollowPlayer>();     
+        follower = transform.GetComponent<FollowPlayer>();
     }
-    
+
     private void Start()
-    {          
+    {
         m_HP.text = "HP : " + m_EnemyHP;
     }
-    
+
     void Update()
-    {    
-            
+    {
+
         //攻擊間隔
-        m_EnemyShootTime -=Time.deltaTime;
-        if(m_EnemyShootTime <= 0)
+        m_EnemyShootTime -= Time.deltaTime;
+        if (m_EnemyShootTime <= 0)
         {
             Attack(Vector3.forward);
             m_EnemyShootTime = 3f;
-        }                  
+        }
     }
-    
+
     void LateUpdate()
     {
         transform.position += Vector3.forward * m_EnemySpeed * Time.deltaTime;
     }
-    
+
     private void OnValidate()
     {
-        
+
     }
-    
+
     protected override void Attack(Vector3 dir)
-    {        
-        Debug.Log("開始射擊");                         
+    {
+        Debug.Log("開始射擊");
 
         // 計算從敵人到玩家的方向
         Vector3 direction = dir.normalized;
@@ -49,14 +49,14 @@ public class Enemy :  IEnemy
         Quaternion rotation = Quaternion.LookRotation(direction);
 
         // 產生子彈並給它正確的朝向
-        Instantiate(m_EnemyBullet, transform.position, rotation);      
-        
+        Instantiate(m_EnemyBullet, transform.position, rotation);
+
     }
 
     protected override void Die() => Destroy(gameObject);
- 
+
     void OnTriggerEnter(Collider other)
-    {       
+    {
         // 判斷來撞(或進入Trigger)的物件是否有子彈標籤
         if (other.TryGetComponent<IBullet>(out IBullet _bullet))
         {
@@ -68,5 +68,17 @@ public class Enemy :  IEnemy
                 Die();
             }
         }
+    }
+
+    void OnDestroy()
+    {
+        UIController.current.OnCoinCollect += CoinCollect;
+        UIController.current.CoinCollect();
+        UIController.current.OnCoinCollect -= CoinCollect;
+    }
+
+    void CoinCollect()
+    {
+        print("coin collect");
     }
 }
